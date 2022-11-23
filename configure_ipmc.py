d3acd322e9a4517ba48455b20351242dbd96437d
Alp Akpinar <aakpinar@bu.edu>
2022-11-23 10:31:07 -0500

    Move socket command IO function to util.py script

--- configure_ipmc.py
+++ configure_ipmc.py
@@ -7,6 +7,8 @@ import yaml
 import socket
 import argparse
 
+from util import write_command_and_read_output
+
 # Check Python version, need at least 3.6
 valid_python = sys.version_info.major >= 3 and sys.version_info.minor >= 6 
 assert valid_python, "You need Python version >=3.6 to run this script!"
@@ -85,45 +87,6 @@ def get_commands(config):
             commands.append(f"{commandbase} {value}\r\n")
 
     return commands
-
-
-def write_command_and_read_output(
-    sock: socket.socket, 
-    command: str,
-    max_size: int=2048,
-    read_until: bytes=b">",
-    ) -> str:
-    """
-    Given the socket interface, writes a command to IPMC TelNet interface 
-    and returns the data echoed back.
-
-    The maximum message size to expect is specified via the max_size argument.
-    """
-    counter = 0
-    data = b""
-
-    # Send the command one byte at a time
-    for ch in command:
-        sock.send(bytes(ch, 'utf-8'))
-        # Read back the command
-        _ = sock.recv(1)
-
-    # Recieve the echoed message one byte at a time
-    while counter < max_size:
-        rcv = sock.recv(1)
-        if rcv == read_until:
-            if counter != 0:
-                break
-            else:
-                continue
-        data += rcv
-        counter += 1
-
-    # Get the leftover ">" and " " characters before exiting
-    for i in range(2):
-        _ = sock.recv(1)
-
-    return data.decode('ascii')
 
 
 def validate_command_output(output, config):
---
+++ util.py
@@ -0,0 +1,43 @@
+import socket
+
+# 
+# Utility functions to be used by the rest of the code are defined here.
+# 
+
+def write_command_and_read_output(
+    sock: socket.socket, 
+    command: str,
+    max_size: int=2048,
+    read_until: bytes=b">",
+    ) -> str:
+    """
+    Given the socket interface, writes a command to IPMC TelNet interface 
+    and returns the data echoed back.
+
+    The maximum message size to expect is specified via the max_size argument.
+    """
+    counter = 0
+    data = b""
+
+    # Send the command one byte at a time
+    for ch in command:
+        sock.send(bytes(ch, 'utf-8'))
+        # Read back the command
+        _ = sock.recv(1)
+
+    # Recieve the echoed message one byte at a time
+    while counter < max_size:
+        rcv = sock.recv(1)
+        if rcv == read_until:
+            if counter != 0:
+                break
+            else:
+                continue
+        data += rcv
+        counter += 1
+
+    # Get the leftover ">" and " " characters before exiting
+    for i in range(2):
+        _ = sock.recv(1)
+
+    return data.decode('ascii')
